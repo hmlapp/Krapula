9acbba376aecd104d4da7b198e3b06ea3382a628
Riku Kuusinen <riku.kuusinen91@gmail.com>
2018-06-18 14:37:53 +0300

    Added classes Item(Weapon, Armor, Food)

---
+++ Krapula/Armor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krapula
+{
+    //Riku
+    class Armor:Item
+    {
+        public int DamageBlock { get; set; }
+        public int Style { get; set; }
+        public Armor(string name, int value, int damageBlock, int style) :base(name, value)
+        {
+            Random arm = new Random(1 - 5);
+            DamageBlock = arm.Next();
+            Style = arm.Next();
+
+            Style = style;
+            DamageBlock = damageBlock;
+        }
+
+        //Vaatteiden nimiä = 
+    }
+}
---
+++ Krapula/Item.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Krapula
+{//Riku ja Nina
+    public class Item
+    {
+        public string Name { get; set; }
+        public int Value { get; set; }
+
+        public Item(string name, int value)
+        {
+           
+            Random r = new Random(1-1000);
+            Value = r.Next();
+            Name = name;
+            Value = value;
+
+        }
+    }
+}
