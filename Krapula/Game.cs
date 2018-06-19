25fb21404bfde3b607c4591be9a26150e587174b
villeimm <ville.immonen2@gmail.com>
2018-06-19 13:17:00 +0300

    inventory print

--- Krapula/Game.cs
+++ Krapula/Game.cs
@@ -20,8 +20,6 @@ namespace Krapula
         {
             player = new Player(name);
             currentArea = new Area();
-            currentArea.SetArea();
-            Console.WriteLine(currentArea.Name);
             pastAreas = new List<Area>();
             IsPlayerAlive = true;
             IsPlayerTurn = true;
@@ -33,7 +31,6 @@ namespace Krapula
             CommandList.Add("hit", Hit);
             CommandList.Add("defend", Defend);
             CommandList.Add("run", Run);
-            CommandList.Add("take", Take);
             CommandList.Add("equip", Equip);
             CommandList.Add("inventory", Inventory);
             CommandList.Add("consume", Consume);
@@ -58,6 +55,8 @@ namespace Krapula
                 {
                     Console.WriteLine(CommandList[cmd[0]]());
                 }
+                
+                IsPlayerTurn = false;
             }
             else
             {
@@ -95,63 +94,7 @@ namespace Krapula
             // Looking at NPC gives information
             throw new NotImplementedException();
         }
-        public string Hit()
-        {
-            if (currentArea.AreaNPC == null)
-            {
-                return "nothing to hit";
-            }
-            currentArea.AreaNPC.Health -= player.Equipped.Damage;
-            if (currentArea.AreaNPC.Health <= 0)
-            {
-                currentArea.AreaNPC = null;
-                return "he ded";
-            }
-            IsPlayerTurn = false;
-            return "you hit the mörkö for " + player.Equipped.Damage + " damage";
-            //Console.WriteLine("Tehty vahinkoa" + (Damage - Armor.DamageBlock + "pistettä");
 
-        }
-        public string Defend()
-        {
-            throw new NotImplementedException();
-            //Nostaa armorin määrän 2x 3 vuoroksi
-
-            //return Equipped.Item.Armor.DamageBlock = Equipped.Item.Armor.DamageBlock* 2; 
-            //        Durability = 3;
-        }
-        public string Run()
-        {
-            // Default return true
-            // Run away from mörkö. Back to previous area
-            // Mörkö can prevent your escape -> return false
-
-            // return true;
-            throw new NotImplementedException();
-        }
-
-        private string Take()
-        {
-            throw new NotImplementedException();
-        }
-
-        public string Equip()
-        {
-            throw new NotImplementedException();
-            //Console.WriteLine(Item.Name + " otettu käyttöön!");
-        }
-
-        public string Inventory()
-        {
-            // Tells the player what items he/she has
-            throw new NotImplementedException();
-
-        }
-        public string Consume()
-        {
-            //Console.WriteLine("Ahmit " Food.Name + "ja terveytesi parani: " + Food.Energy + "verran!");
-            throw new NotImplementedException();
-        }
         public string Buy()
         {
             // Buy target item
@@ -170,6 +113,60 @@ namespace Krapula
             // Add item to NPC inventory
             throw new NotImplementedException();
         }
+
+        public string Run()
+        {
+            // Default return true
+            // Run away from mörkö. Back to previous area
+            // Mörkö can prevent your escape -> return false
+
+            // return true;
+            throw new NotImplementedException();
+        }
+
+        public string Inventory()
+        {
+            List<Item> foods = new List<Item>();
+            List<Item> weapons = new List<Item>();
+            List<Item> drinks = new List<Item>();
+            List<Item> clothes = new List<Item>();
+
+            StringBuilder items = new StringBuilder();
+
+            // Tells the player what items he/she has
+            foreach (var item in player.Inventory)
+            {
+                if (item.Equals(typeof(Food)))
+                {
+                    foods.Add(item);
+                }
+                else if (item.Equals(typeof(Weapon)))
+                {
+                    weapons.Add(item);
+                }
+                else if (item.Equals(typeof(Armor)))
+                {
+                    clothes.Add(item);
+                }
+            }
+
+
+            Console.WriteLine("Takataskustasi löytyy:");
+            Console.WriteLine();
+            return items.ToString();
+
+        }
+
+        public string Consume()
+        {
+            //Console.WriteLine("Ahmit " Food.Name + "ja terveytesi parani: " + Food.Energy + "verran!");
+            throw new NotImplementedException();
+        }
+        public string Drink()
+        {
+            //Console.WriteLine("Juotuasi: " Food.Name + " ja terveytesi parani: " + Food.Energy + "verran!");
+            throw new NotImplementedException();
+        }
         //if(Health != MaxHealth){
         //public override int Health
         //{
@@ -178,6 +175,41 @@ namespace Krapula
         //if(Food.Name == "ES"){
         ////return Equipped.Item.Weapon.Damage = Equipped.Item.Weapon.Damage* 2;
 
+        public string Equip()
+        {
+            throw new NotImplementedException();
+            //Console.WriteLine(Item.Name + " otettu käyttöön!");
+        }
 
+        public string Hit()
+        {
+            if (currentArea.AreaNPC == null)
+            {
+                return "nothing to hit";
+            }
+            currentArea.AreaNPC.Health -= player.Equipped.Damage;
+            if (currentArea.AreaNPC.Health <= 0)
+            {
+                currentArea.AreaNPC = null;
+                return "he ded";
+            }
+            return "you hit the mörkö for " + player.Equipped.Damage + " damage";
+            //Console.WriteLine("Tehty vahinkoa" + (Damage - Armor.DamageBlock + "pistettä");
+
+        }
+        public string Defend()
+        {
+            throw new NotImplementedException();
+            //Nostaa armorin määrän 2x 3 vuoroksi
+
+            //return Equipped.Item.Armor.DamageBlock = Equipped.Item.Armor.DamageBlock* 2; 
+            //        Durability = 3;
+        }
+
+        public void Take(Item item)
+        {
+            player.Inventory.Add(item);
+            Console.WriteLine("Poimit eeppiset lootit ja takataskussai on nyt {0}", item);
+        }
     }
 }
