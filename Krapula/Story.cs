b3c3fb53e3d28573a39e05434e8fb4fd1039c94e
npuh <ninapuhakka@hotmail.com>
2018-06-20 09:50:38 +0300

    Testing

--- Krapula/Story.cs
+++ Krapula/Story.cs
@@ -9,8 +9,11 @@ namespace Krapula
 {//Nina ja Riku
     public class Story
     {
+       
+
        public static void Beginning()
       {
+         
             Console.WriteLine("Heräät juhannuksen jälkeen kaameassa krapulassa.");
             System.Threading.Thread.Sleep(3000);
             Console.WriteLine("Suusi on tahmea, päähän sattuu ja jossain haisee oksennus."  );
@@ -37,6 +40,49 @@ namespace Krapula
 
             return Utilities.RandomStringFrom(path).Trim() + " " + npc;
         }
+        public static void Ending()
+        {
+            Console.WriteLine("Paha mörkö voitti sinut.. snif.");
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("Mutta ei hätää. Seikkailit tyylikkäillä vaatteilla, taistelit parhaasi mukaan ja päällesi jäi: ");  //equippedarmor);
+            System.Threading.Thread.Sleep(3000);
+            Console.WriteLine("");
+            System.Threading.Thread.Sleep(3000);
 
+            int pisteet = 50;
+            switch (pisteet)
+            {
+                case int n when (n >= 60):
+                    Console.WriteLine("Wau. Huipputulos!! Oletko harjoitellut kotiin selviämistä aiemminkin? ");
+
+                    break;
+                case int n when (n < 59 && n >= 30):
+                    Console.WriteLine("Pelasit hyvin.. ish.");
+                    break;
+                case int n when (n < 29 && n >= 0):
+                    Console.WriteLine("Höh, et oikein pärjännyt tällä kertaa. Muista nesteyttää itseäsi ennen peliä ja yritä uudelleen! ");
+                    break;
+                default:
+                    Console.WriteLine("Pelasit sitten meidän peliä.");
+                    break;
+            }
+
+                   Console.WriteLine("Haluatko pelata uudelleen? (Kyllä/Ei)");
+            string vastaus= Console.ReadLine().ToLower();
+            switch (vastaus)
+            {
+                case string s when (vastaus == "kyllä" || vastaus == "yes" || vastaus == "k"):
+                    Console.WriteLine("Jee uudestaan.");
+                    break;
+                case string s when (vastaus == "ei" || vastaus == "no" || vastaus == "e"):
+                    Console.WriteLine("Fiksu päätös.");
+                    break;                
+                default:
+                    Console.WriteLine("Anna kunnollinen vastaus");
+                    break; 
+            }
+
+            
+        }
     }
 }
